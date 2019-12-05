d3fd0035be0245683a1413ca66df6483c69385ed
olegstat <olegstat1992@gmail.com@>
2019-12-05 20:20:32 +0200

    lesson 7 solutions

---
+++ lesson_7.py
@@ -0,0 +1,47 @@
+#1
+
+from functools import reduce
+a = [1,2,3,4,5,6]
+b = reduce(lambda x, y : x+y, filter(lambda x: x % 2 == 0, a))
+print(b)
+
+#2
+
+def my_generator(n):
+    while n > 0:
+        yield n*n
+        n -= 1
+a = []
+for i in my_generator(10):
+    a.append(i)
+a.sort()
+print(a)
+
+#3
+
+l = [1, 2, [3, 4, [5, 6]], 7, 8, [9, [10]]] 
+output = [] 
+def flatter(n): 
+    for i in n: 
+        if type(i) == list: 
+            flatter(i) 
+        else: 
+            output.append(i) 
+flatter(l) 
+print(output) 
+
+#4
+
+l = [1, 2, [3, 4, [5, 6]], 7, 8, [9, [10]]]
+f = lambda x: x*x
+output = [] 
+def flatter(n,m): 
+    for i in n: 
+        if type(i) == list: 
+            flatter(i,m) 
+        else: 
+            output.append(m(i)) 
+flatter(l,f) 
+print(output) 
+
+
