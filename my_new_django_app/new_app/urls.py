384b280affc72d83c2fe7a8991e0dca005f8fc2c
olegstat <olegstat1992@gmail.com@>
2019-12-17 23:23:42 +0200

    lesson 12 solutions

--- my_new_django_app/new_app/urls.py
+++ my_new_django_app/new_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from new_app import views
 
 urlpatterns = [
+    path('file_saver/', views.file_saver),
+    path('get_sc_pt/', views.get_sc_pt),
+    path('get_username/', views.get_username),
     path('content/', views.content),
     path('ny_days/', views.ny_days),
     path('random/', views.random_numb),
