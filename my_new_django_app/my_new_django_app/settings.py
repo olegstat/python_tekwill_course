e6d72a031c00eb8513bba53a9542c0047efb7cfc
olegstat <olegstat1992@gmail.com@>
2019-12-25 15:51:51 +0200

    lesson 12,13,14 solutions

--- my_new_django_app/my_new_django_app/settings.py
+++ my_new_django_app/my_new_django_app/settings.py
@@ -106,14 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = False
-
+USE_L10N = True
 USE_TZ = False
 
 
